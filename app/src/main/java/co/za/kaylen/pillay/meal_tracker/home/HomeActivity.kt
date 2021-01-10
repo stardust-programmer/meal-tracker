04e0fc7d2266c520334b268edbd92e6053aabad9
Kaylen <stardust.programmer@gmail.com>
2021-01-10 14:40:53 +0200

    - Added initial home screen layout and activity

--- app/src/main/java/co/za/kaylen/pillay/meal_tracker/home/HomeActivity.kt
+++ app/src/main/java/co/za/kaylen/pillay/meal_tracker/home/HomeActivity.kt
@@ -1,6 +1,13 @@
 package co.za.kaylen.pillay.meal_tracker.home
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import co.za.kaylen.pillay.meal_tracker.R
 
 class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home_activity_layout)
+    }
 }
